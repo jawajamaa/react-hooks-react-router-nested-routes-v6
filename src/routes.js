057407a9845efeffc1f61e5bf91c722d3d3883e2
Timothy Ryon <tim@timryon.com>
2024-02-21 21:39:07 -0500

    modify routes

--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,37 @@
-import Home from "./pages/Home";
+import App from "./App";
 import About from "./pages/About";
+import ErrorPage from "./pages/ErrorPage";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import UserProfile from "./pages/UserProfile";
-import ErrorPage from "./pages/ErrorPage";
 
 const routes = [
   {
     path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />
-  }, 
-  {
-    path: "/about",
-    element: <About />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/login",
-    element: <Login />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/profile/:id",
-    element: <UserProfile />,
-    errorElement: <ErrorPage />
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        errorElement: <ErrorPage />
+      }, 
+      {
+        path: "/about",
+        element: <About />,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: "/login",
+        element: <Login />,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: "/profile/:id",
+        element: <UserProfile />,
+        errorElement: <ErrorPage />
+      }
+    ]
   }
 ];
 
